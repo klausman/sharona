67d9d557aebf1033e064721a03649e3ec4e1723a
Tobias Klausmann <klausman@schwarzvogel.de>
2023-09-10 13:28:14 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sharona
+
+This tool converts MC framework simple assignGear files to code to make the
+same items appear in an ACE Limited Arsenal.
+
+The EXE file is standalone (run from desktop or wherever) and allows you to
+paste the file contents, and then produces the SQF for a limited arsenal. Paste
+that code into the init of a box in-game and you're done.
+
+This program is a drop-in successor of https://github.com/klausman/qtlola since
+the Qt Bindings maintained by TheRecipe are not suitable for me anymore.
+
+License: GPL v2
---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"regexp"
+	"sort"
+	"strings"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/widget"
+)
+
+const (
+	version = "0.0.5"
+)
+
+var (
+	reComment = regexp.MustCompile(`//.*$`)
+	reLoadout = regexp.MustCompile(`(?i).*setunitloadout *(\[[^;]+);.*`)
+	reQitem   = regexp.MustCompile(`.*("[^"]+").*`)
+
+	versioninfo = fmt.Sprintf("Sharona v%s\n© 2023 Tobias Klausmann\n\nSharona converts simple assignGear loadouts to ACE limited arsenals.\nhttps://github.com/klausman/sharona", version)
+
+	showv = flag.Bool("v", false, "Show version number and exit")
+)
+
+func main() {
+	flag.Parse()
+
+	if *showv {
+		fmt.Println(versioninfo)
+		os.Exit(0)
+	}
+
+	a := app.New()
+	w := a.NewWindow("Sharona")
+	w.Resize(fyne.NewSize(500, 500))
+
+	ab := fyne.NewMenuItem("About", func() { dialog.ShowInformation("About", versioninfo, w) })
+
+	fm := fyne.NewMenu("File", ab)
+	mm := fyne.NewMainMenu(fm)
+	w.SetMainMenu(mm)
+
+	input := makeEntry("Enter text...")
+	output := makeEntry("Output will appear here")
+
+	c := make(chan string)
+	button := widget.NewButton("Convert", func() {
+		c <- input.Text
+	})
+	go func() {
+		for {
+			select {
+			case t := <-c:
+				output.SetText(getLAfromLO(t))
+			}
+		}
+	}()
+	boxes := container.NewAdaptiveGrid(1, input, output)
+	content := container.NewBorder(button, nil, nil, nil, boxes)
+	w.SetContent(content)
+
+	w.ShowAndRun()
+}
+
+func makeEntry(pht string) *widget.Entry {
+	w := widget.NewEntry()
+	w.MultiLine = true
+	w.SetMinRowsVisible(10)
+	w.TextStyle.Monospace = true
+	w.SetPlaceHolder(pht)
+	return w
+}
+
+func getLAfromLO(s string) string {
+	items := make(map[string]bool)
+	for _, line := range strings.Split(s, "\n") {
+		line = strings.Trim(line, " \n\r\t")
+		line = reComment.ReplaceAllString(line, "")
+		lo := reLoadout.FindStringSubmatch(line)
+		if len(lo) == 0 {
+			continue
+		}
+		for _, tok := range strings.Split(lo[1], ",") {
+			stripped := reQitem.FindStringSubmatch(tok)
+			if len(stripped) == 0 {
+				continue
+			}
+			items[stripped[1]] = true
+		}
+	}
+	itemlist := make([]string, 0, len(items))
+	for k := range items {
+		itemlist = append(itemlist, k)
+	}
+	sort.Strings(itemlist)
+	return fmt.Sprintf("[this, [\n    %s]\n] call ace_arsenal_fnc_initBox;\n", strings.Join(itemlist, ",\n    "))
+}
